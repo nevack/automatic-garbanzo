bcb95e16e8ce786fa5ba1bc7d92e3ce957fb9635
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-01 16:07:30 +0300

    Day1 Part1 Solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            val left = line.first { it.isDigit() }.digitToInt()
+            val right = line.last { it.isDigit() }.digitToInt()
+            sum += left * 10 + right
+        }
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
 }

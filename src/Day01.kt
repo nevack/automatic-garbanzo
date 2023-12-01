e61202701c28934cc27d8e51a12914dd9e498597
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-01 16:16:04 +0300

    Day1 Part2 Solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,9 +9,44 @@ fun main() {
         return sum
     }
 
+    fun part2(input: List<String>): Int {
+        var sum = 0
+        for (line in input) {
+            val left = line.findAnyOf(scanMap.keys)!!.second.scanMap()
+            val right = line.findLastAnyOf(scanMap.keys)!!.second.scanMap()
+            sum += left * 10 + right
+        }
+        return sum
+    }
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
+    part2(input).println()
 }
+
+private val scanMap = mapOf(
+    "zero" to 0,
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+    "0" to 0,
+    "1" to 1,
+    "2" to 2,
+    "3" to 3,
+    "4" to 4,
+    "5" to 5,
+    "6" to 6,
+    "7" to 7,
+    "8" to 8,
+    "9" to 9,
+)
+
+private fun String.scanMap(): Int = scanMap[this]!!

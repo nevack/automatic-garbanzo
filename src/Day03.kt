f412ed529b121bcb1d38b0b94481ebc1c9d7e764
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-04 01:01:33 +0300

    Day3 Part2 Solved

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,13 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return GridScanner(input).process()
+        return GridScanner(input).process().first
+    }
+
+    fun part2(input: List<String>): Int {
+        return GridScanner(input).process().second
     }
 
     val testInput1 = readInput("Day03_test1")
     check(part1(testInput1) == 4361)
+    val testInput2 = readInput("Day03_test1")
+    check(part2(testInput2) == 467835)
 
     val input = readInput("Day03")
     part1(input).printlnPrefix("Part1 answer")
+    part2(input).printlnPrefix("Part2 answer")
 }
 
 class GridScanner(
@@ -29,10 +36,12 @@ class GridScanner(
         scannerLines = sequenceOf(firstRow) + input.asSequence().map { ".$it." } + sequenceOf(lastRow)
     }
 
-    fun process(): Int {
+    fun process(): Pair<Int, Int> {
+        val gears = mutableMapOf<Pair<Int, Int>, MutableSet<Int>>()
+
         var sum = 0
         val scanWindow = scannerLines.windowed(size = SIZE)
-        scanWindow.forEach { rows ->
+        scanWindow.forEachIndexed { windowIndex, rows ->
             val numbers = rows[1].numbers()
 
             for ((number, range) in numbers) {
@@ -44,6 +53,11 @@ class GridScanner(
                             if (!scanned.isDigit() && scanned != '.') {
                                 found = true
                             }
+                            if (scanned == '*') {
+                                gears.getOrPut((i + windowIndex) to (j + index - 1)) {
+                                    mutableSetOf()
+                                }.add(number)
+                            }
                         }
                     }
                 }
@@ -53,7 +67,11 @@ class GridScanner(
             }
         }
 
-        return sum
+        val power: Int = gears.values.filter { gear -> gear.size >= 2 }.sumOf { numbers ->
+            numbers.fold(1) { a: Int, b: Int -> a * b }
+        }
+
+        return sum to power
     }
 
     private fun String.numbers(): Sequence<Pair<Int, IntRange>> {

47035b993fd194de9b1721bfe612cee5cd9f7adf
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-04 19:28:55 +0300

    Day4 Part2 Solved

--- src/Day04.kt
+++ src/Day04.kt
@@ -17,11 +17,29 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        val cards = input.map { parseLine(it) }
+        val counts = MutableList(cards.size) { 1 }
+
+        cards.forEachIndexed { i, card ->
+            val count = card.have.intersect(card.winning).size
+            for (next in 1..count) {
+                counts[i + next] += counts[i]
+            }
+        }
+
+        return counts.sum()
+    }
+
     val testInput1 = readInput("Day04_test1")
     check(part1(testInput1) == 13)
 
+    val testInput2 = readInput("Day04_test1")
+    check(part2(testInput2) == 30)
+
     val input = readInput("Day04")
     part1(input).printlnPrefix("Part1 answer")
+    part2(input).printlnPrefix("Part2 answer")
 }
 
 class Card(

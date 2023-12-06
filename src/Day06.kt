e270c42f0b53427ea7785406207ce7cfd6cac867
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-06 20:38:05 +0300

    Day6 Part2 Solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,8 +2,8 @@
 fun main() {
     fun parse(input: List<String>): List<Race> {
         check(input.size == 2)
-        val times = input[0].substringAfter("Time:     ").split(' ').filter { it.isNotEmpty() }.map { it.toInt() }
-        val dists = input[1].substringAfter("Distance: ").split(' ').filter { it.isNotEmpty() }.map { it.toInt() }
+        val times = input[0].substringAfter("Time:     ").split(' ').filter { it.isNotEmpty() }.map { it.toLong() }
+        val dists = input[1].substringAfter("Distance: ").split(' ').filter { it.isNotEmpty() }.map { it.toLong() }
         check(times.size == dists.size)
 
         val races = times.mapIndexed { index, time ->
@@ -12,6 +12,14 @@ fun main() {
         }
 
         return races
+    }
+
+    fun parse2(input: List<String>): Race {
+        check(input.size == 2)
+        val time = input[0].substringAfter("Time:     ").split(' ').filter { it.isNotEmpty() }.joinToString("").toLong()
+        val dist = input[1].substringAfter("Distance: ").split(' ').filter { it.isNotEmpty() }.joinToString("").toLong()
+
+        return Race(time, dist)
     }
 
     fun part1(input: List<String>): Int {
@@ -24,14 +32,25 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        val race = parse2(input)
+
+        return (1..race.time).filter { time ->
+            race.dist < time * (race.time - time)
+        }.size
+    }
+
     val testInput1 = readInput("Day06_test1")
     check(part1(testInput1) == 288)
+    val testInput2 = readInput("Day06_test1")
+    check(part2(testInput2) == 71503)
 
     val input = readInput("Day06")
     part1(input).printlnPrefix("Part1 answer")
+    part2(input).printlnPrefix("Part1 answer")
 }
 
 data class Race(
-    val time: Int,
-    val dist: Int,
+    val time: Long,
+    val dist: Long,
 )

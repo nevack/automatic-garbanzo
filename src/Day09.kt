8e4672f8a773b1c647f322d385a3df91d12b6237
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-10 02:57:34 +0300

    Day9 Cleanup

--- src/Day09.kt
+++ src/Day09.kt
@@ -3,38 +3,30 @@ fun main() {
         return line.split(" ").map { it.toInt() }
     }
 
-    fun part1(input: List<String>): Int {
-        return input.map { line -> parseLine(line) }.sumOf { progression ->
-            val last = mutableListOf<Int>()
-            var level = progression
+    fun solve(progression: List<Int>): Int {
+        val last = mutableListOf<Int>()
+        var level = progression
 
-            while (!level.all { it == 0 }) {
-                last += level.last()
-                level = level.windowed(2) { it[1] - it[0] }
-            }
-
-            last.sum()
+        while (!level.all { it == 0 }) {
+            last += level.last()
+            level = level.windowed(2) { it[1] - it[0] }
         }
+
+        return last.sum()
     }
 
-    fun part2(input: List<String>): Long {
-        return input.map { line -> parseLine(line) }.sumOf { progression ->
-            val first = mutableListOf<Int>()
-            var level = progression
+    fun part1(input: List<String>): Int {
+        return input.map { line -> parseLine(line) }.sumOf { progression -> solve(progression) }
+    }
 
-            while (!level.all { it == 0 }) {
-                first += level.first()
-                level = level.windowed(2) { it[1] - it[0] }
-            }
-
-            first.reversed().fold(0L) { acc, next -> next - acc }
-        }
+    fun part2(input: List<String>): Int {
+        return input.map { line -> parseLine(line).reversed() }.sumOf { progression -> solve(progression) }
     }
 
     val testInput1 = readInput("Day09_test1")
     check(part1(testInput1) == 114)
     val testInput2 = readInput("Day09_test1")
-    check(part2(testInput2) == 2L)
+    check(part2(testInput2) == 2)
 
     val input = readInput("Day09")
     part1(input).printlnPrefix("Part1 answer")

1a0acae6cb0b8abbdb5b8127c65812acc6f80249
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-10 02:52:19 +0300

    Day9 Part2 Solved

--- src/Day09.kt
+++ src/Day09.kt
@@ -17,9 +17,26 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Long {
+        return input.map { line -> parseLine(line) }.sumOf { progression ->
+            val first = mutableListOf<Int>()
+            var level = progression
+
+            while (!level.all { it == 0 }) {
+                first += level.first()
+                level = level.windowed(2) { it[1] - it[0] }
+            }
+
+            first.reversed().fold(0L) { acc, next -> next - acc }
+        }
+    }
+
     val testInput1 = readInput("Day09_test1")
     check(part1(testInput1) == 114)
+    val testInput2 = readInput("Day09_test1")
+    check(part2(testInput2) == 2L)
 
     val input = readInput("Day09")
     part1(input).printlnPrefix("Part1 answer")
+    part2(input).printlnPrefix("Part2 answer")
 }

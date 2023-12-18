6c9e0c42e16cba3c67fd3422cfb5ac6074390e1b
Dzmitry Neviadomski <nevack.d@gmail.com>
2023-12-19 00:05:52 +0300

    Day18 Part2 Solved

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,7 +1,7 @@
 import kotlin.math.abs
 
 fun main() {
-    fun parseLine(line: String): Pair<Direction, Int> {
+    fun parseLine1(line: String): Pair<Direction, Int> {
         val (directionStr, countStr, _) = line.split(" ")
         val direction = when(directionStr) {
             "R" -> Direction.RIGHT
@@ -15,27 +15,46 @@ fun main() {
         return direction to count
     }
 
-    fun part1(input: List<String>): Int {
-        val moves = input.map { line -> parseLine(line) }
+    fun parseLine2(line: String): Pair<Direction, Int> {
+        val hexStr = line.split(" ").last().removeSurrounding("(#", ")")
+        val direction = Direction.entries[hexStr.last().digitToInt()]
+        val count = hexStr.dropLast(1).toInt(16)
 
-        val exterior = moves.sumOf { (_, count) -> count }
-        var interior = 0
+        return direction to count
+    }
+
+    fun solve(moves: List<Pair<Direction, Int>>): Long {
+        val exterior = moves.sumOf { (_, count) -> count.toLong() }
+        var interior = 0L
 
         moves.fold(0 to 0) { cur, (direction, count) ->
             val next = cur + (direction * count)
-            interior += cur cross next
+            interior += (next.second - cur.second).toLong() * (next.first + next.first)
             next
         }
 
         return abs(interior) / 2 + exterior / 2 + 1
     }
 
+    fun part1(input: List<String>): Long {
+        val moves = input.map { line -> parseLine1(line) }
+
+        return solve(moves)
+    }
+
+    fun part2(input: List<String>): Long {
+        val moves = input.map { line -> parseLine2(line) }
+
+        return solve(moves)
+    }
+
     val testInput1 = readInput("Day18_test1")
-    check(part1(testInput1) == 62)
+    check(part1(testInput1) == 62L)
+    check(part2(testInput1) == 952408144115L)
 
     val input = readInput("Day18")
     timed("Part1 answer") { part1(input) }
-    // timed("Part2 answer") { part2(input) }
+    timed("Part2 answer") { part2(input) }
 }
 
 private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> {
@@ -44,8 +63,4 @@ private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> 
 
 private operator fun Direction.times(x: Int): Pair<Int, Int> {
     return di * x to dj * x
-}
-
-private infix fun Pair<Int, Int>.cross(other: Pair<Int, Int>): Int {
-    return (other.second - second) * (other.first + first)
 }
